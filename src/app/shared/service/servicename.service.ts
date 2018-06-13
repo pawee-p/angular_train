7ebd37a636c7fa016a9d3d8692d94e04944cdc76
Test <abc@gmail.com>
2018-06-13 16:19:12 +0700

    Finished

--- src/app/shared/service/servicename.service.ts
+++ src/app/shared/service/servicename.service.ts
@@ -14,7 +14,7 @@ export class ServicenameService {
 
    loadProject(): Observable<any[]> {
 
-     return this._http.get(`${environment.API_URL}/api/v1/project`).pipe( map( (resp) => {
+     return this._http.get(`${environment.API_URL}/api/v1/project`).pipe(map((resp) => {
        return resp.json();
      } ) ).pipe(  catchError( value => Observable.throw(value))  );
    }
@@ -25,7 +25,7 @@ export class ServicenameService {
          'userCode' : user.userId,
          'userPwd' : user.password
        };
-       return this._http.post(`${environment.API_URL}/api/v1/login/doLogin`, body).pipe( map((resp) => {
+       return this._http.post(`${environment.API_URL}/api/v1/login/doLogin`, body).pipe(map((resp) => {
         return resp.json();
        }));
    }
